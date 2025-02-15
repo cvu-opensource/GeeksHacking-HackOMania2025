bf9f4991d24628ae659a2fb994850b29dd5a20f3
ben <bengoh04@gmail.com>
2025-02-16 07:51:32 +0800

    the yym guide

--- vectordb/entrypoint.sh
+++ vectordb/entrypoint.sh
@@ -3,6 +3,6 @@
 # 3 seperate commands 
 ollama serve & \
 ollama run ${EMBEDDER} & \
-ollama run ${LLM} & \
+# ollama run ${LLM} & \
 python3 MainControllerVectorDB.py & \
-python3 MainControllerEventsDB.py \
+# python3 MainControllerEventsDB.py \
